9018fd2a523bc2da935e80849be4bbf03f6de0b1
ZwTz <jdangro4@gmail.com>
2024-11-23 13:08:38 -0600

    Patrocinador torneo

--- gest_Deport/resources/views/torneo/create.blade.php
+++ gest_Deport/resources/views/torneo/create.blade.php
@@ -4,22 +4,26 @@
 @endsection
 
 @section('content')
-    
+
 <div class="results-table">
     <div class="form-title">Registrar Torneo</div>
     <form class="player-form" action="{{ route('torneo.store') }}" method="POST">
         @csrf
         <label>Nombre Torneo</label>
         <input type="text" name="nombre_torneo" required>
+        <label for="patrocinador_torneo">Patrocinador del Torneo</label>
+        <input type="text" id="patrocinador_torneo" name="patrocinador_torneo" >
+        <br class="jump">
+
+        <label for="monto_patrocinador">Monto patrocinador</label>
+            <input type="number" id="monto_patrocinador" name="monto_patrocinador" >
+        <br class="jump">
+
 
         <label>Tipo de Torneo</label>
         <input type="text" name="tipo_torneo" required>
 
-        <label>Numero de Equipos</label><!---->
-        <!--
-        jotillo
-        
-        -->
+        <label>Numero de Equipos</label>
         <input type="int" name="numero_equipos" required>
 
         <label for="deporte_id">Deporte</label>
