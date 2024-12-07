791a9cadb3b9471ea9d79c76e37aae740aded138
ZwTz <jdangro4@gmail.com>
2024-12-07 12:42:55 -0600

    jale lo de sebas

--- gest_Deport/app/Http/Controllers/TorneoController.php
+++ gest_Deport/app/Http/Controllers/TorneoController.php
@@ -9,25 +9,26 @@ use App\Models\Torneo;
 use App\Models\Jugador;
 use App\Models\Equipo;
 
-
 class TorneoController extends Controller
 {
-
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nombre_torneo' => 'required|string|max:20',
-            'patrocinador_torneo' => 'nullable|string|max:20',
-            'monto_patrocinador' => 'nullable|integer|min:0|max:1000000',
-            'numero_equipos' => 'required|integer|in:4,8,16',
-        ], [
-            'nombre_torneo.required' => 'El nombre del torneo es obligatorio.',
-            'nombre_torneo.max' => 'El nombre del torneo no puede exceder los 20 caracteres.',
-            'numero_equipos.in' => 'El número de equipos debe ser 4, 8 o 16.',
-        ]);
+        $validatedData = $request->validate(
+            [
+                'nombre_torneo' => 'required|string|max:20|unique:torneos,nombre_torneo',
+                'patrocinador_torneo' => 'nullable|string|max:20',
+                'monto_patrocinador' => 'nullable|integer|min:0|max:1000000',
+                'numero_equipos' => 'required|integer|in:4,8,16',
+            ],
+            [
+                'nombre_torneo.required' => 'El nombre del torneo es obligatorio.',
+                'nombre_torneo.max' => 'El nombre del torneo no puede exceder los 20 caracteres.',
+                'nombre_torneo.unique' => 'El nombre del torneo ya existe. Por favor, elija otro.',
+                'numero_equipos.in' => 'El número de equipos debe ser 4, 8 o 16.',
+            ],
+        );
 
         try {
-
             Torneo::create([
                 'nombre_torneo' => $request->nombre_torneo,
                 'patrocinador_torneo' => $request->patrocinador_torneo ?? 'Sin patrocinador',
@@ -41,7 +42,6 @@ class TorneoController extends Controller
         }
     }
 
-
     public function edit($id)
     {
         $torneo = Torneo::findOrFail($id);
@@ -50,23 +50,24 @@ class TorneoController extends Controller
 
     public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
-            'nombre_torneo' => 'required|string|max:20',
-            'patrocinador_torneo' => 'nullable|string|max:20',
-            'monto_patrocinador' => 'nullable|integer|min:0|max:1000000',
-        ], [
-            'nombre_torneo.required' => 'El nombre del torneo es obligatorio.',
-            'nombre_torneo.max' => 'El nombre del torneo no puede exceder los 20 caracteres.',
-        ]);
-
+        $validatedData = $request->validate(
+            [
+                'nombre_torneo' => 'required|string|max:20|unique:torneos,nombre_torneo,' . $id,
+                'patrocinador_torneo' => 'nullable|string|max:20',
+                'monto_patrocinador' => 'nullable|integer|min:0|max:1000000',
+            ],
+            [
+                'nombre_torneo.required' => 'El nombre del torneo es obligatorio.',
+                'nombre_torneo.max' => 'El nombre del torneo no puede exceder los 20 caracteres.',
+                'nombre_torneo.unique' => 'El nombre del torneo ya existe. Por favor, elija otro.',
+            ],
+        );
 
         $torneo = Torneo::findOrFail($id);
         $torneo->update([
             'nombre_torneo' => $request->nombre_torneo,
             'patrocinador_torneo' => $request->patrocinador_torneo ?? 'Sin patrocinador',
             'monto_patrocinador' => $request->monto_patrocinador ?? 0,
-
-
         ]);
         return redirect()->route('torneo.read')->with('success', 'Torneo actualizado con éxito');
     }
@@ -93,8 +94,7 @@ class TorneoController extends Controller
     public function buscar(Request $request)
     {
         $nombre = $request->input('nombre');
-        $torneos = Torneo::where('nombre_torneo', 'LIKE', "%$nombre%")
-            ->get();
+        $torneos = Torneo::where('nombre_torneo', 'LIKE', "%$nombre%")->get();
         return response()->json($torneos);
     }
 
@@ -110,8 +110,7 @@ class TorneoController extends Controller
     public function mostrarTorneo($torneoId)
     {
         // Obtiene el torneo con sus partidos, equipos relacionados y los agrupa por ronda
-        $torneo = Torneo::with(['partidos.equipoLocal', 'partidos.equipoVisitante', 'partidos.instalacion'])
-            ->findOrFail($torneoId);
+        $torneo = Torneo::with(['partidos.equipoLocal', 'partidos.equipoVisitante', 'partidos.instalacion'])->findOrFail($torneoId);
 
         // Agrupa los partidos por ronda
         $partidosPorRonda = $torneo->partidos->groupBy('ronda');
@@ -133,7 +132,6 @@ class TorneoController extends Controller
         // Pasar los datos a la vista
         return view('torneo.detalle', compact('torneo', 'partidosPorRonda', 'ganador'));
     }
-
 
     // Función para obtener partidos por ronda
     private function getPartidosPorRonda($torneo)
@@ -184,9 +182,8 @@ class TorneoController extends Controller
         $torneo->save();
 
         // Redirigir a la ruta arbitro.dashboard con el mensaje de éxito
-        return redirect()->route('arbitro.dashboard')
+        return redirect()
+            ->route('arbitro.dashboard')
             ->with('success', 'Torneo finalizado correctamente. El ID del ganador del último partido es: ' . ($id_ganador ? $id_ganador : 'No disponible'));
     }
 }
-
-
